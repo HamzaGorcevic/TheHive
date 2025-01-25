72d665a78204559cd495b2ecf7fe2eec9b8b5c50
HamzaGorcevic <gorcevichamza76@gmail.com>
2025-01-25 16:02:50 +0100

    restricted reserve option for users

--- hive_client/src/views/users/viewUser.jsx
+++ hive_client/src/views/users/viewUser.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axiosClient from "../../axios";
 import styles from "./viewUser.module.scss";
@@ -6,13 +6,14 @@ import UserCard from "../profile/user";
 import BeekeeperCard from "../profile/beekeeper";
 import UserServicesList from "../profile/userServices";
 import CustomLoader from "../../components/loader/loader";
+import StateContext from "../../contexts/authcontext";
 
 const ViewUser = () => {
     const { user_id } = useParams();
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-
+    const { authData } = useContext(StateContext);
     useEffect(() => {
         const fetchUserProfile = async () => {
             try {
@@ -43,7 +44,9 @@ const ViewUser = () => {
                         beekeeper={user.beekeeper}
                         isViewMode={true}
                     />
-                    <UserServicesList userId={user_id} isViewMode={true} />
+                    {authData?.user?.role != "user" && (
+                        <UserServicesList userId={user_id} isViewMode={true} />
+                    )}{" "}
                 </>
             )}
         </div>
